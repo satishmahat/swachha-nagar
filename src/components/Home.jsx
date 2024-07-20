c752f87f312a563fb5c8f936b2db2056f48ee459
Sandiya Thapa <123964434+Sandiya23@users.noreply.github.com>
2024-07-20 14:48:36 +0545

    home added

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+
+    
+
+<section class="bg-center bg-no-repeat bg-[url('src/assets/photo-1650638987536-6fbcb9bc6085.jpeg')] bg-gray-500 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 bg-transparent">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-300 md:text-5xl lg:text-9xl bg-transparent">स्वच्छ नगर </h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-4xl sm:px-16 lg:px-48 bg-transparent">“Your waste, our responsibility. Let’s keep our city clean."</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 bg-transparent">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white lg:text-xl rounded-2xl bg-[#009600] hover:bg-[#55b355] focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Join Us Now
+                
+            </a>
+            
+        </div>
+    </div>
+</section>
+
+    </div>
+  )
+}
+
+export default Home
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Footer from "../components/Footer"
+import Home from "../components/Home"
 import Navbar from "../components/Navbar"
 
 
@@ -6,6 +7,7 @@ function HomePage(){
     return(<>
         <Navbar/>
 
+        <Home/>
 
 
         <Footer/>
